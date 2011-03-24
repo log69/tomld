cbd107fa8887957580d49f04917e1d35af417ce0
Andras Horvath <han@log69.com>
2011-03-24 20:02:56 +0100

    add changelog

--- tomld.py
+++ tomld.py
@@ -29,6 +29,7 @@
 #                          - set maximum accept entry value in profile.conf to a predefined one
 #                          - add --once switch to quit after first cycle immediately (might be useful for scripts)
 #                          - don't print confirmation text at the end of log messages with --yes on
+#                          - don't print sleeping period with --once on
 # 23/03/2011 - tomld v0.17 - add --keep switch to run tomld without turning learning mode domains into enforcing mode
 #                          - add --recursive switch to mark subdirs of these dirs fully wildcarded (usable for samba shares for example)
 #                          - on rules with allow_create entries, create the same with allow_unlink and allow_read/write too
