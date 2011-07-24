44cb9542f043d2adb6c30661c449146c8ceccb6e
Andras Horvath <han@log69.com>
2011-07-24 11:46:55 +0200

    remove config backup

--- grub.sh
+++ grub.sh
@@ -3,11 +3,9 @@
 # linux has to start with "security=tomoyo" kernel parameter to activate tomoyo
 
 GRUB_DEFAULT="/etc/default/grub"
-TDOMF="/etc/tomoyo/domain_policy.conf"
-TEXCF="/etc/tomoyo/exception_policy.conf"
 
 # add kernel parameter to grub config
-echo "* searching for security=tomoyo kernel parameter in grub config"
+echo "* checking grub config"
 if ! grep -E "^ *GRUB_CMDLINE_LINUX" "$GRUB_DEFAULT" | grep -q "security=tomoyo"
 then
 	if grep -qE "^ *GRUB_CMDLINE_LINUX *=" "$GRUB_DEFAULT"
@@ -33,8 +31,3 @@ then
 else
 	echo "* kernel parameter already set"
 fi
-
-# backup tomoyo config
-echo "* backing up original tomoyo config"
-test ! -f "$TDOMF" || mv "$TDOMF" "$TDOMF".bak.installer.$(date +%s)
-test ! -f "$TEXCF" || mv "$TEXCF" "$TEXCF".bak.installer.$(date +%s)
