07e371a8f464bb0a44c51ac840f4e16776cdd6c8
Andras Horvath <han@log69.com>
2011-09-01 09:51:58 +0200

    add function string_sort_lines()

--- tstring.h
+++ tstring.h
@@ -121,6 +121,8 @@ char *string_remove_line_from_pos(char *text, unsigned int pos);
 
 int string_cmp(const void *a, const void *b);
 
+char *string_sort_lines(const char *text);
+
 char *string_sort_uniq_lines(const char *text);
 
 char *array_copy_to_string_list(char **array);
