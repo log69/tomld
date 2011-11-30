c9344cc975d001a494ce4790bff50a39b4720d9c
Andras Horvath <han@log69.com>
2011-11-30 19:08:26 +0100

    update timestamp

--- tomld.c
+++ tomld.c
@@ -742,7 +742,7 @@ void version() {
 	printf ("Copyright (C) 2011 Andras Horvath\n");
 	printf ("E-mail: mail@log69.com - suggestions & feedback are welcome\n");
 	printf ("URL: http://log69.com - the official site\n");
-	printf ("(last update Wed Nov 30 13:21:50 CET 2011)\n"); /* last update date c23a662fab3e20f6cd09c345f3a8d074 */
+	printf ("(last update Wed Nov 30 18:56:50 CET 2011)\n"); /* last update date c23a662fab3e20f6cd09c345f3a8d074 */
 	printf ("\n");
 	printf ("LICENSE:\n");
 	printf ("This program is free software; you can redistribute it and/or modify it ");
